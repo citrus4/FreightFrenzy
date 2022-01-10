cee8651f6ef46b7d223b17d23a395d7109e54b49
ABAdkins <a.brown46464@gmail.com>
2022-01-10 10:59:00 -0600

    retuning rr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,10 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.04;
-    public static double kA = 0.006;
+    public static double kV = 1.0 / rpmToVelocity(MAX_RPM); //0.04
+    public static double kA = 0; //0.006
     public static double kStatic = 0;
-
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
      * the constraints should never exceed ~80% of the robot's actual capabilities. While Road
